f0cefc335689a43ab64f2f40cea3362170667ac3
Magdalena Nolde <magdalena@nolde.de>
2022-01-16 11:55:58 +0100

    finish task

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,10 +2,7 @@ const ulExcursions = document.querySelector('.panel__excursions');
 const liPrototypeExcursion = document.querySelector('.excursions__item--prototype');
 const liPrototypeBasket = document.querySelector('.summary__item--prototype');
 const ulBasket = document.querySelector('.panel__summary');
-// const uploader = document.querySelector('.uploader__input');
 const titles = [];
-const totalPriceArray = [];
-const cart = []
 const totalSumSpan = document.querySelector(".order__total-price-value");
 totalSumSpan.textContent = " ";
 const nameField = document.querySelector("input[name=name]");
@@ -13,7 +10,8 @@ const surnameField = document.querySelector("input[name=surname]");
 const emailField = document.querySelector("input[name=email]");
 const submitBtn = document.querySelector(".order__field-submit");
 const ulCart = document.querySelector(".panel__summary");
-console.log(ulCart);
+cart = [];
+
 
 // LADOWANIE WYCIECZEK
 
@@ -57,8 +55,6 @@ function createExcursion(lines){
             newExcursion.querySelector(".excursions__description").textContent = lines[i][5];
             let adultInput = newExcursion.querySelector("input[name=adults]");
             adultInput.previousElementSibling.textContent = lines[i][7];
-            newExcursion.classList.remove("excursions__item--prototype");
-            newExcursion.classList.add("excursions__item--new");
             adultInput.previousElementSibling.setAttribute('title', "adultsPrice");
             let childInput = newExcursion.querySelector("input[name=children]");
             childInput.previousElementSibling.textContent = lines[i][9];
@@ -71,90 +67,120 @@ function createExcursion(lines){
 
 // WYBIERANIE WYCIECZEK
 
-ulExcursions.addEventListener("click", addToCart);
+ulExcursions.addEventListener("click", getValues);
 
-function addToCart (e){
-    if(e.target.type === "submit") {
-        const newExcursions = document.querySelectorAll(".excursions__item--new");
-        e.preventDefault();
-        newExcursions.forEach(function(excursion) {
-            const adultField = excursion.querySelector("input[name=adults]");
-            const childField = excursion.querySelector("input[name=children]");
-            const title = excursion.querySelector(".excursions__title");
-            if(adultField.value > 0 || childField.value > 0) {
-                let newBasketItem = liPrototypeBasket.cloneNode(true);
-                ulBasket.appendChild(newBasketItem);
-                newBasketItem.style.display = "block";
-                const newTitle = newBasketItem.querySelector(".summary__name");
-                newTitle.textContent = title.textContent;
-                    if(!titles.includes(newTitle.textContent)) {
-                        titles.push(newTitle.textContent);
-                        cart.push(newBasketItem);
-                        validateExcursionFields(adultField, childField);
-                        const pricesSummary = newBasketItem.querySelector(".summary__prices");
-                        const childPrice = excursion.querySelector("[title=childrenPrice]").textContent;
-                        const adultPrice = excursion.querySelector("[title=adultsPrice]").textContent;
-                        const finalPrice = calculatePriceExcurion(adultField.value, adultPrice, childField.value, childPrice);
-                        totalPriceArray.push(finalPrice);
-                        const totalPrice = calculateSum(totalPriceArray);
-                        totalSumSpan.textContent = totalPrice + "PLN";
-                        pricesSummary.textContent = "Dorośli:" + " " + `${adultField.value}` + "x" + " " + adultPrice + "PLN" + "," + " " + "Dzieci:" + " " + `${childField.value}` + "x" + childPrice + "PLN";
-                        newBasketItem.querySelector(".summary__total-price").textContent = finalPrice + " " + "PLN"
-                        newBasketItem.classList.remove("summary.item--prototype");
-                        newBasketItem.setAttribute('title', `${name}`);
-                        
-                    } else {
-                        newBasketItem.remove();
-                    } 
-                    excursion.querySelector("form").reset();
-            }
-        })   
+function addItem(titleInput, adultNumber, adultPrice, childNumber, childPrice) {
+    let duplicateExists = false
+    for(i = 0; i < cart.length; i++) {
+       if(cart[i].title === titleInput) {
+            duplicateExists = true
+            break
+        }
     }
+        if(!duplicateExists) cart.push({title: titleInput, adultNumber: adultNumber, adultPrice: adultPrice, childNumber: childNumber, childPrice: childPrice});
+        createBasketItem();
 }
 
-function calculatePriceExcurion(num1, num2, num3, num4) {
+
+function getValues(e) {
+    e.preventDefault();
+    if(e.target.classList.contains('excursions__field-input--submit')) {
+        const parentElement = e.target.parentElement.parentElement.parentElement;
+        const adultNumber = parentElement.querySelector("input[name=adults]").value;
+        const childNumber = parentElement.querySelector("input[name=children]").value;
+        const titleInput = parentElement.querySelector(".excursions__title").textContent;
+        const childPrice = parentElement.querySelector("[title=childrenPrice]").textContent;
+        const adultPrice = parentElement.querySelector("[title=adultsPrice]").textContent;
+        addItem(titleInput, adultNumber, adultPrice, childNumber, childPrice)
+        parentElement.querySelector("form").reset();
+    }      
+} 
+
+function createBasketItem() {
+    cart.forEach(function(item) {
+        let newBasketItem = liPrototypeBasket.cloneNode(true);
+        ulBasket.appendChild(newBasketItem);
+        newBasketItem.style.display = "block";
+        const newTitle = newBasketItem.querySelector(".summary__name");
+        const pricesSummary = newBasketItem.querySelector(".summary__prices");
+        newTitle.textContent = item.title;
+            if(!titles.includes(newTitle.textContent)) {
+                titles.push(newTitle.textContent);
+                const validedChildNumber = validateExcursionFields(item.childNumber);
+                const validedAdultdNumber = validateExcursionFields(item.adultNumber);
+                pricesSummary.textContent = "Dorośli:" + " " + `${validedAdultdNumber}` + "x" + " " + `${item.adultPrice}` + "PLN" + "," + " " + "Dzieci:" + " " + `${validedChildNumber}` + "x" + `${item.childPrice}` + "PLN";
+                newBasketItem.querySelector(".summary__total-price").textContent = getItemTotal(item.childNumber,item.childPrice, item.adultNumber,item.adultPrice) + " " + "PLN";
+                newBasketItem.classList.add('summary__item--new');
+                newBasketItem.querySelector("img").setAttribute('title', `${item.title}`);
+            }
+            else {newBasketItem.remove();
+            }
+    })
+    totalSumSpan.textContent = getfinalTotal() + " " + "PLN";
+    console.log(titles);
+} 
+
+function convertNumber (num) {
+    const convertedNum = parseFloat(num);
+    return convertedNum;
+}
+
+function isCorrectNumber (num) {
+    if(Number.isNaN(Number(num))) {
+        return false;
+    }
+    return true;
+}
+        
+
+function validateExcursionFields (num) {
+    if (num === "") {
+        num = 0;
+        }
+    const convertednum = convertNumber(num);
+    if(!isCorrectNumber(convertednum)) {
+        alert ("Mozesz podac tylko liczbe. Jedna z wartosci nie jest liczba");
+    }
+    return convertednum;
+}
+
+function getItemTotal (num1, num2, num3, num4) {
     let result = (num1 * num2) + (num3 * num4)
     return result;
 }
 
-function validateExcursionFields (field1, field2) {
-    if (field1.value === "") {
-        field1.value = 0;
-        }
-    if (field2.value === "") {
-        field2.value = 0;
-        }
-    if((Number.isNaN(Number(field1.value))) || (Number.isNaN(Number(field2.value)))) {
-            alert("musisz podac liczbe");
-        }
+function getfinalTotal() {
+    let total = 0
+    for (let i = 0; i < cart.length; i += 1) {
+      total += getItemTotal(cart[i].adultNumber, cart[i].adultPrice, cart[i].childNumber, cart[i].childPrice);
+    }
+    return total
 }
-
-function calculateSum (arr) {
-    if(!Array.isArray(arr)) return;
-    return arr.reduce((a, v)=>a + v);
-}
-
 
 // USUWANIE WYCIECZEK
 
 ulCart.addEventListener("click", removeExcursion);
 
 function removeExcursion (e) {
-    if (e.target.tagName === 'IMG') {
-            cart.forEach(function(element){ 
-            const name = element.querySelector(".summary__name").textContent;
-            console.log(name);
-            if(element.name === element.title.name) {
-                element.remove();}
-            })
-    } 
+    if (e.target.classList.contains('summary__btn-remove')) {
+        for (let i = 0; i < cart.length; i += 1) 
+            if (cart[i].title === e.target.title) {
+            cart.splice(cart[i], 1);
+            e.target.parentElement.parentElement.remove();
+        }
+        //  for (let i = 0; i < titles.length; i += 1)
+            // if(titles[i] === e.target.title) {
+            // titles.splice(titles[i], 1);
+    createBasketItem();
+    }
 }
 
-// ZAMAWIANIE WYCIECZEK
+
+// WYPELNIENIE FORMULARZA
 
 submitBtn.addEventListener("click", validateOrderForm);
 
-function validateOrderForm (e, form) {
+function validateOrderForm (e) {
     const errors = []
   
     if (!emailField.value.includes('@')) {
@@ -185,9 +211,11 @@ function validateOrderForm (e, form) {
      
     if(errors.length === 0) {
         e.preventDefault();
-        e.stoppropagation;
         if(cart.length > 0) {
+            deleteBasket();
+            document.querySelector(".panel__order").reset();
             alert("Dziękujemy za złożenie zamówienia o wartości" + " " + `${totalSumSpan.textContent}` + " " + "Szczegóły zamówienia zostały wysłane na adres e-mail: adres@wpisanywformularzu.pl");
+            totalSumSpan.textContent = " ";
         } else {
             alert("Jeszcze nie wybrales zadnej wycieczki. Zapoznaj sie z naszym katalogiem przez wgranie pliku");
         }
@@ -198,6 +226,82 @@ function validateOrderForm (e, form) {
 function changeBackSettings(el) {
     el.style.border = "2px inset grey"
 }
+
+function deleteBasket () {
+    const liList = ulBasket.querySelectorAll(".summary__item--new");
+    liList.forEach(function(li) {
+        li.remove();
+    });
+ }
+// 
+
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
